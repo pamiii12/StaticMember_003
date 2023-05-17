4f8b0ec85bcd5408ff49bfa8558b8cc79389dbc0
pamiii12 <fahmiadityaa03@gmail.com>
2023-05-17 16:21:40 +0700

    func

--- konstrukktor/konstrukktor.cpp
+++ konstrukktor/konstrukktor.cpp
@@ -14,3 +14,21 @@ public:
 	void cetak();
 
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
